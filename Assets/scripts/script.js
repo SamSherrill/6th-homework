7cbd31c55fcd856a7471ca343a6ce52896198bc2
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-05-14 20:52:50 -0400

    folder & file skeleton built out, document ready. Style and script pulled in.

---
+++ Assets/scripts/script.js
@@ -0,0 +1,8 @@
+//=========================================================================================
+// API KEY: in ENV file
+// example call: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=78ebcf5b45dec15369fc1101ca8eeb5d
+//=========================================================================================
+// ```
+$(document).ready(function () {
+    console.log("Document ready")
+});
