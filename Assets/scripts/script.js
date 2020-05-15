99a040c9035ec9121ffd9e7203276df33dcab9c9
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-05-15 09:56:45 -0400

    added api usage instructions, other small tweaks

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,9 +1,14 @@
-// API KEY: in ENV file
-// example query URL: api.openweathermap.org/data/2.5/weather?q=atlanta,ga,us&APPID=API_KEY
+// example query URL: api.openweathermap.org/data/2.5/weather?q=atlanta,ga,us&APPID=7d8227fc067bf4e67f91ead9ca584dc2
 
 $(document).ready(function () {
     console.log("Document ready");
     $("#five-day-forecast").prepend(
         $("<h3>5 Day Forecast:</h3>" + "<div class='d-block'>")
     );
+
+    $("#city-submit-button").on("click", function (event) {
+                event.preventDefault();
+                var cityName = $("#city-input").val();
+                console.log(`cityName: ${cityName}`)
+    });
 });
