7df51739f6d0061aec88f2dded567a1cd5dc8797
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-05-14 21:56:23 -0400

    getting some initial jQuery to populate

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,8 +1,9 @@
-//=========================================================================================
 // API KEY: in ENV file
-// example call: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=78ebcf5b45dec15369fc1101ca8eeb5d
-//=========================================================================================
-// ```
+// example query URL: api.openweathermap.org/data/2.5/weather?q=atlanta,ga,us&APPID=API_KEY
+
 $(document).ready(function () {
-    console.log("Document ready")
+    console.log("Document ready");
+    $("#five-day-forecast").prepend(
+        $("<h3>5 Day Forecast:</h3>" + "<div class='d-block'>")
+    );
 });
